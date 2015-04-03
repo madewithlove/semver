49b6d6f14264bbcefcc873f567f92c88ae0d2aed
Maxime Fabre <ehtnam6@gmail.com>
2015-04-03 17:08:11 +0200

    Every goddamn time

--- resources/views/_partials/footer.php
+++ resources/views/_partials/footer.php
@@ -1,5 +1,5 @@
 <footer>
     &copy; <?= date('Y') ?> -
-    <a href="https://github.com/madewithlove/semver">View sources</a> -
+    <a target="_blank" href="https://github.com/madewithlove/semver">View sources</a> -
     <a target="_blank" href="http://madewithlove.be">madewithlove</a>
 </footer>
