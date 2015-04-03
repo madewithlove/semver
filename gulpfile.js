9460b20f7cd6bf99839f07f20de2a80b624942a3
Maxime Fabre <ehtnam6@gmail.com>
2015-04-03 13:50:56 +0200

    Setup front-end stack

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp       = require('gulp'),
+		changed    = require('gulp-changed'),
+		concat     = require('gulp-concat'),
+		config     = require('./gulpconfig.json'),
+		cssmin     = require('gulp-cssmin'),
+		livereload = require('gulp-livereload'),
+		plumber    = require('gulp-plumber'),
+		prefix     = require('gulp-autoprefixer'),
+		rename     = require('gulp-rename'),
+		annotate   = require('gulp-ng-annotate'),
+		sourcemaps = require('gulp-sourcemaps'),
+		uglify     = require('gulp-uglify');
+
+// Configuration
+//////////////////////////////////////////////////////////////////////
+
+gulp.task('css', function () {
+	return gulp.src(config.styles)
+		.pipe(plumber())
+		.pipe(changed(config.paths.compiled.css))
+		.pipe(prefix())
+		.pipe(sourcemaps.init())
+			.pipe(concat('styles.css'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(config.paths.compiled.css))
+		.pipe(livereload());
+});
+
+gulp.task('js', function () {
+	return gulp.src(config.scripts)
+		.pipe(plumber())
+		.pipe(changed(config.paths.compiled.js))
+		.pipe(sourcemaps.init())
+			.pipe(annotate())
+			.pipe(concat('scripts.js'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(config.paths.compiled.js))
+		.pipe(livereload());
+});
+
+gulp.task('minify', function () {
+	gulp.src(config.paths.compiled.css + '/styles.css')
+		.pipe(plumber())
+		.pipe(cssmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(config.paths.compiled.css));
+
+	gulp.src(config.paths.compiled.js + '/scripts.js')
+		.pipe(plumber())
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(config.paths.compiled.js));
+});
+
+// Tasks
+//////////////////////////////////////////////////////////////////////
+
+gulp.task('default', ['css', 'js']);
+
+gulp.task('watch', function () {
+	livereload.listen();
+
+	gulp.watch(config.paths.original.js + '/**/*.js', ['js']);
+	gulp.watch(config.paths.original.views + '/**/*.php', function () {
+		livereload.reload('index.php');
+	});
+});
---
+++ public/app/js/app.js
@@ -0,0 +1,5 @@
+var app = angular.module('semver', []);
+
+app.controller('AppController', function ($scope, $http) {
+
+});
