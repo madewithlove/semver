881ba2cd2ee511a7ef7abae749e5843e54ca5105
Maxime Fabre <ehtnam6@gmail.com>
2015-04-03 17:35:47 +0200

    Move search button

--- resources/views/index.php
+++ resources/views/index.php
@@ -4,8 +4,8 @@
     <section class="search">
         <form ng-submit="fetchVersions()" ng-init="fetchVersions()">
             <input placeholder="Package" id="package" type="text" ng-model="package">
+            <button type="submit" class="btn btn-default">Search</button>
             <input placeholder="Version" id="version" type="text" ng-model="version" ng-change="fetchMatchingVersions()">
-            <button type="submit" class="btn btn-default">Fetch</button>
         </form>
 
         <p class="error" ng-show="versions.length && errors.versions">The package {{ package }} does not exist</p>
