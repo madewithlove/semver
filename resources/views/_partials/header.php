336ffac9b919a51762c2d70220cc344d3fc328db
Maxime Fabre <ehtnam6@gmail.com>
2015-04-03 16:29:20 +0200

    Add link for more infos

--- resources/views/_partials/header.php
+++ resources/views/_partials/header.php
@@ -1,3 +1,6 @@
 <header class="header">
     <h1>Packagist Semver Checker</h1>
+    <p>
+        <a target="_blank" href="https://getcomposer.org/doc/01-basic-usage.md#package-versions">Learn more about version constraints</a>
+    </p>
 </header>
