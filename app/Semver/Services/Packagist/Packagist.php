57d51f5dac8627291dcc05dac0e9d4cf84f6a36f
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2015-04-07 10:04:56 +0200

    Added method to get default constraint that would be added to composer.json require block when installing given package

--- app/Semver/Services/Packagist/Packagist.php
+++ app/Semver/Services/Packagist/Packagist.php
@@ -1,8 +1,11 @@
 <?php
 namespace Semver\Services\Packagist;
 
+use Composer\DependencyResolver\Pool;
 use Composer\Package\LinkConstraint\VersionConstraint;
+use Composer\Package\Package;
 use Composer\Package\Version\VersionParser;
+use Composer\Package\Version\VersionSelector;
 use Packagist\Api\Client;
 use Packagist\Api\Result\Package\Version;
 
@@ -70,6 +73,41 @@ class Packagist
         return array_keys(array_filter($versions, function (Version $version) use ($constraint) {
             return $constraint->matches(new VersionConstraint('==', $this->parser->normalize($version->getVersion())));
         }));
+    }
+
+    /**
+     * Given a concrete package, this returns a ~ constraint (in the future a ^ constraint)
+     * that should be used, for example, in composer.json.
+     *
+     * For example:
+     *  * 1.2.1         -> ~1.2
+     *  * 1.2           -> ~1.2
+     *  * v3.2.1        -> ~3.2
+     *  * 2.0-beta.1    -> ~2.0@beta
+     *
+     * @param string $vendor
+     * @param string $package
+     *
+     * @return string
+     */
+    public function getDefaultConstraint($vendor, $package)
+    {
+        // Get versions.
+        $versions = array_keys($this->getRawVersions($vendor, $package));
+
+        // Get highest version.
+        $highestVersion = reset($versions);
+        foreach ($versions as $version) {
+            if (version_compare($highestVersion, $version, '<')) {
+                $highestVersion = $version;
+            }
+        }
+
+        // Let version selector format the constraint.
+        $selector = new VersionSelector(new Pool());
+        $package = new Package("$vendor/$package", $this->parser->normalize($highestVersion), $highestVersion);
+
+        return $selector->findRecommendedRequireVersion($package);
     }
 
     /**
