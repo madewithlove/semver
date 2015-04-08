30e937c2f00de13525fa5d79442d0d56cc16815f
Maxime Fabre <ehtnam6@gmail.com>
2015-04-08 21:57:14 +0200

    Move controller to separate file

---
+++ public/app/js/Controllers/AppController.js
@@ -0,0 +1,66 @@
+angular.module('semver').controller('AppController', function ($scope, $http, $location) {
+
+	$scope.package = $location.search().package || 'madewithlove/elasticsearcher';
+	$scope.defaultVersion = '~1.2.3';
+	$scope.version = $location.search().version || '^0.1.1';
+	$scope.errors = {
+		versions: false,
+		matching: false,
+	};
+
+	$scope.versions = [];
+	$scope.matchingVersions = [];
+
+	/**
+	 * Fetches all versions of the specified package
+	 */
+	$scope.fetchVersions = function () {
+		$http.get('/packages/' + $scope.package).success(function (response) {
+			$scope.versions = response.versions;
+			$scope.version = $scope.defaultVersion = response.default_constraint;
+			$scope.errors.versions = false;
+
+			$scope.fetchMatchingVersions();
+		}).error(function () {
+			$scope.errors.versions = true;
+		});
+	};
+
+	/**
+	 * Fetches all versions matching a specified range
+	 */
+	$scope.fetchMatchingVersions = function () {
+		if (!$scope.version) {
+			return;
+		}
+
+		// Update URL
+		$location.search({
+			package: $scope.package,
+			version: $scope.version,
+		});
+
+		$http.get('/packages/' + $scope.package + '/match', {
+			params: {
+				constraint: $scope.version
+			}
+		}).success(function (versions) {
+			$scope.matchingVersions = versions;
+			$scope.errors.matching = false;
+		}).error(function () {
+			$scope.errors.matching = true;
+		});
+	};
+
+	/**
+	 * Checks if a version is within the specified version range
+	 *
+	 * @param {string} version
+	 *
+	 * @returns {boolean}
+	 */
+	$scope.matches = function (version) {
+		return $scope.matchingVersions.indexOf(version) !== -1;
+	};
+
+});
