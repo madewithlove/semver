3ba8a0a34979ded5d396292e02b3ffc4856839a4
Tony Messias <tonyzrp@gmail.com>
2015-08-07 18:46:54 -0300

    packagist service first test

--- tests/SemVer/Services/Packagist/PackagistTest.php
+++ tests/SemVer/Services/Packagist/PackagistTest.php
@@ -4,6 +4,8 @@ namespace Semver\Services\Packagist;
 
 use Composer\Package\Version\VersionParser;
 use Mockery;
+use Packagist\Api\Result\Package\Source;
+use Packagist\Api\Result\Package\Version;
 use PHPUnit_Framework_TestCase;
 
 class PackagistTest extends PHPUnit_Framework_TestCase
@@ -29,9 +31,48 @@ class PackagistTest extends PHPUnit_Framework_TestCase
 		Mockery::close();
 	}
 
-	/** @test */
-	function it_works()
+	/**
+	 * @return array
+	 */
+	private function buildVersions()
 	{
-		$this->assertInstanceOf(Packagist::class, $this->service);
+		$source = new Source();
+		$source->fromArray(['url' => 'http://lorem.ipsum']);
+
+		$firstVersion = new Version();
+		$firstVersion->fromArray([
+			'source' => $source,
+			'version' => '0.2.2'
+		]);
+
+		$secondVersion = new Version();
+		$secondVersion->fromArray([
+			'source' => $source,
+			'version' => '0.2.1'
+		]);
+
+		return [
+			'0.2.1' => $secondVersion,
+			'0.2.2' => $firstVersion,
+		];
+	}
+
+	/** @test */
+	function it_gets_sorted_versions()
+	{
+		$vendor = 'madewithlove';
+		$package = 'elasticsearcher';
+		$packageName = "{$vendor}/{$package}";
+		$versions = $this->buildVersions();
+
+		$this->repository->shouldReceive('getVersions')
+			->with($packageName)
+			->andReturn($versions);
+
+		$result = $this->service->getVersions('madewithlove', 'elasticsearcher');
+
+		$this->assertCount(2, $result);
+		$this->assertEquals($versions['0.2.2']->getVersion(), $result[0]['version']);
+		$this->assertEquals($versions['0.2.1']->getVersion(), $result[1]['version']);
 	}
 }
