b65cd5e75eff6f1c9872ce78d5dec32380b75811
Maxime Fabre <ehtnam6@gmail.com>
2016-04-29 16:02:23 +0200

    Use hardlink for projects-banner

--- public/app/js/index.js
+++ public/app/js/index.js
@@ -1,6 +1,5 @@
 import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import '../sass/styles.scss';
-import 'projects-banner';
 import angular from 'angular';
 
 angular.module('semver', []);
