75b43198db96ec4b42aef46af025fc9632b4a16a
woutersioen <wouter.sioen@gmail.com>
2021-02-05 15:24:07 +0100

    Verify stability to check if version matches constraint as well

--- app/Version/Matcher.php
+++ app/Version/Matcher.php
@@ -2,6 +2,7 @@
 
 namespace App\Version;
 
+use Composer\Package\BasePackage;
 use Composer\Semver\Constraint\Constraint;
 use Composer\Semver\VersionParser;
 
@@ -14,12 +15,34 @@ final class Matcher
         $this->versionParser = $versionParser;
     }
 
-    public function matches(string $version, string $constraint, string $stability): bool
+    public function matches(string $version, string $constraint, string $requiredStability): bool
     {
         $constraint = $this->versionParser->parseConstraints($constraint);
+        $parsedVersion = new Constraint('=', $this->versionParser->normalize($version));
 
-        return $constraint->matches(
-            new Constraint('=', $this->versionParser->normalize($version))
-        );
+        if (!$parsedVersion->matches($constraint)) {
+            return false;
+        }
+
+        if (!$this->isMoreOrEquallyStable($version, $requiredStability)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Helper method.
+     *
+     * @param string $version
+     * @param string $requiredStability
+     *
+     * @return bool
+     */
+    private function isMoreOrEquallyStable($version, $requiredStability)
+    {
+        $stability = $this->versionParser->parseStability($version);
+
+        return BasePackage::$stabilities[$stability] <= BasePackage::$stabilities[$requiredStability];
     }
 }
