e18f609ba9f46fbbba322df140ded85e2316b322
Jonas Drieghe <jonasd@madewithlove.be>
2021-10-22 18:48:47 +0200

    Enforce iterable types in non-framework code

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,15 +8,6 @@ use Illuminate\Support\Facades\Gate;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
-
-    /**
      * Register any authentication / authorization services.
      *
      * @return void
