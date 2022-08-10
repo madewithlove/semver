20ac4fdd53e4416354993895b23c4878d0764778
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2022-08-10 09:31:56 +0300

    Fix package comparison (not nullable)

--- resources/views/livewire/semver-checker.blade.php
+++ resources/views/livewire/semver-checker.blade.php
@@ -15,7 +15,7 @@
                 id="package"
                 type="text"
                 autofocus=""
-                class="@if ($package === null) border-red-500 @endif flex-grow text-center border-2 border-gray-400 bg-gray-100 p-3 text-gray-400 focus:text-gray-500 outline-none mr-3 text-lg font-bold"
+                class="@if ($package === '') border-red-500 @endif flex-grow text-center border-2 border-gray-400 bg-gray-100 p-3 text-gray-400 focus:text-gray-500 outline-none mr-3 text-lg font-bold"
             >
             <input wire:model="constraint" placeholder="Version (eg. ^1.5)" id="version" type="text"  class="@if (!$this->isValidConstraint) border-red-500 @endif flex-grow text-center border-2 border-gray-400 bg-gray-100 p-3 text-gray-400 focus:text-gray-500 outline-none mr-3 text-lg font-bold">
             @unless($isVirtual)
