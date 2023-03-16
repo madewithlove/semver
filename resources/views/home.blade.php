c25f1239b9640911f326709db1122ef01154a771
Andreas Creten <andreas@madewithlove.be>
2023-03-16 14:00:47 +0100

    Fix canonical

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="canonical" href="http://semver.mwl.be">
+        <link rel="canonical" href="https://semver.madewithlove.com">
         <title>Package Semver Checker - madewithlove</title>
         <link rel="icon" type="image/png" href="{{asset('img/favicon-16x16.png')}}" sizes="16x16">
         <link rel="icon" type="image/png" href="{{asset('img/favicon-32x32.png')}}" sizes="32x32">
