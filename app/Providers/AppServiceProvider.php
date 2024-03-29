62c42d2a28a76280c211c922ded8a5ff39f50015
Shift <shift@laravelshift.com>
2024-03-29 09:44:41 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,10 +6,15 @@ use App\Packagist\CachedApiClient;
 use App\Packagist\Client;
 use App\VirtualPackages\VirtualPackageEnrichingApiClient;
 use App\VirtualPackages\VirtualPackageFactory;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public const HOME = '/home';
+
     /**
      * Register any application services.
      *
@@ -41,5 +46,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+        $this->bootRoute();
+    }
+
+    public function bootRoute(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            /** @phpstan-ignore-next-line */
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+
     }
 }
