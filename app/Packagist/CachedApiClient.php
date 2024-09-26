ed31fd91a31b66b90395bc11bba7ea67775d8dfb
jdrieghe <jonasd@madewithlove.com>
2024-09-26 08:26:50 +0200

    Ignore cache remember type issue

--- app/Packagist/CachedApiClient.php
+++ app/Packagist/CachedApiClient.php
@@ -15,6 +15,7 @@ final readonly class CachedApiClient implements Client
 
     public function getPackage(string $packageName): ?Package
     {
+        /** @phpstan-ignore-next-line */
         return Cache::remember('package-' . $packageName, 60 * 60, function () use ($packageName): ?Package {
             return $this->client->getPackage($packageName);
         });
@@ -22,6 +23,7 @@ final readonly class CachedApiClient implements Client
 
     public function search(string $name): array
     {
+        /** @phpstan-ignore-next-line */
         return Cache::remember('search-' . $name, 60 * 60, function () use ($name): array {
             return $this->client->search($name);
         });
