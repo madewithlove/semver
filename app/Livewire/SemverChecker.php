110e71b929dfd4a1b47bb01c4580840fa8f68ce9
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2024-11-20 20:03:28 +0300

    Correct return type

--- app/Livewire/SemverChecker.php
+++ app/Livewire/SemverChecker.php
@@ -10,7 +10,6 @@ use App\Version\Parser;
 use App\Version\Version;
 use App\VirtualPackages\VirtualPackage;
 use App\VirtualPackages\VirtualPackageVersion;
-use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Str;
 use Livewire\Component;
@@ -30,7 +29,7 @@ class SemverChecker extends Component
      */
     protected $queryString = ['package', 'constraint', 'stability'];
 
-    public function render(Client $client, Matcher $matcher): Factory|View
+    public function render(Client $client, Matcher $matcher): View
     {
         $package = $client->getPackage($this->package);
         $versions = [];
