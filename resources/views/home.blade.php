b35687777df465155d146feaab540c3edcb53c13
Yannick <yannick@madewithlove.be>
2025-04-02 16:19:15 +0200

    Adjust links in footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,7 +40,8 @@
             <div class="w-full mx-auto container flex flex-wrap items-center justify-between pt-5 mb-7 text-xs px-5">
                 <div>
                     <span>&copy; {{ now()->year }} <strong>made</strong>with<strong>love</strong></span>
-                    <a href="https://madewithlove.com/disclaimer/?ref=semver" class="ml-5">terms of service</a>
+                    <a href="https://madewithlove.com/cookie-policy/?ref=semver" class="ml-5">cookie policy</a>
+                    <a href="https://madewithlove.com/privacy-policy/?ref=semver" class="ml-5">privacy policy</a>
                     <a target="_blank" href="https://github.com/madewithlove/semver" class="ml-5">source</a>
                     <a target="_blank" href="https://app.changehub.io/share/p-oXqP9gaQg1" class="ml-5">changelog</a>
                     <a target="_blank" href="https://github.com/madewithlove/semver/graphs/contributors" class="ml-5">creators</a>
